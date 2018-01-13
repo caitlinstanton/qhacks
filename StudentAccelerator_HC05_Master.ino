e808f515928f9e170da1f2c7688455096d1557e4
Caitlin Stanton <caitlinstanton44@gmail.com>
2018-01-13 11:04:52 -0800

    Arduino files

---
+++ StudentAccelerator_HC05_Master.ino
@@ -0,0 +1,35 @@
+//  Student Accelerator Hackathon - 2018
+//  HC-05 Basic program to send value (0 or 1) triggered by a button   
+ 
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(9,10); // Arduino RX 9 - BT TX 9  | Arduino TX 10 - BT RX 10 
+
+//button to pin 3
+int button = 3; 
+
+void setup() 
+{  
+    //Serial monitor (top right icon) set to 9600 baud rate
+    Serial.begin(9600);
+    Serial.println("Arduino Serial Begin");
+
+    //HC-05 communication set at 9600 baud rate
+    BTserial.begin(9600);  
+    Serial.println("Bluetooth Serial Begin");
+
+    //pin 3 is set to input information from button 
+    pinMode(button, INPUT); 
+}
+ 
+void loop()
+{
+ //read value from button(pin 3) and send via Bluetooth 
+ BTserial.print(digitalRead(button)); 
+ 
+ //read value from button(pin 3) and send to serial monitor 
+ Serial.println(digitalRead(button));
+
+ //delay 50milliseconds 
+ delay(50);  
+}
+
---
+++ StudentAccelerator_HC06_Slave.ino
@@ -0,0 +1,49 @@
+//  Student Accelerator Hackathon - 2018
+//  HC-06 Basic program to receive value (0 or 1) to turn an LED on/off 
+ 
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(9,10); // Arduino RX 9 - BT TX 10 | Arduino TX 10 - BT RX 9
+
+//LED to pin 3
+int LED = 3; 
+
+//BTValue stores value read from Bluetooth
+int BTValue = 0; 
+
+void setup()  
+{
+    //Serial monitor (top right icon) set to 9600 baud rate  
+    Serial.begin(9600);
+    Serial.println("Arduino Serial Begin");
+ 
+    // HC-06 default baud rate is 9600
+    BTSerial.begin(9600);  
+    Serial.println("Bluetooth Serial Begin");
+
+    // pin 3 is set to output information to LED 
+    pinMode(LED, OUTPUT); 
+}
+ 
+void loop()
+{
+ 
+// check HC-06, read HC-06, trigger LED, and send to Arduino Serial Monitor
+if (BTSerial.available()){
+  
+  //Read from HC-06 and store in BTValue
+  BTValue = BTSerial.read();// values received as characters, use ASCII Table
+    //Compare BTValue with ASCII of '0' ('0' => 48)
+    if (BTValue == 48){
+      //turn LED (pin 3) on
+      digitalWrite(LED,LOW);
+     //Compare BTValue with ASCII of '1' ('1' => 49) 
+    }else if (BTValue == 49){
+      //turn LED (pin 3) off
+      digitalWrite(LED,HIGH);
+    }
+ //print BTValue to Arduino Serial Monitor (top right icon)
+ Serial.print("Value: ");
+ Serial.println(BTValue);
+ 
+}
+}

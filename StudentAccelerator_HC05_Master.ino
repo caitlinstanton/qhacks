5e26e66ef7cbb59ab9fa5cae929a783bd75133a1
Dustin <dustinnewman98@gmail.com>
2018-01-13 11:19:00 -0800

    initial

--- StudentAccelerator_HC05_Master.ino
+++ StudentAccelerator_HC05_Master.ino
@@ -1,35 +1,63 @@
-//  Student Accelerator Hackathon - 2018
-//  HC-05 Basic program to send value (0 or 1) triggered by a button   
- 
-#include <SoftwareSerial.h>
-SoftwareSerial BTserial(9,10); // Arduino RX 9 - BT TX 9  | Arduino TX 10 - BT RX 10 
 
-//button to pin 3
-int button = 3; 
+#define PROCESSING_VISUALIZER 1
+#define SERIAL_PLOTTER  2
 
-void setup() 
-{  
-    //Serial monitor (top right icon) set to 9600 baud rate
-    Serial.begin(9600);
-    Serial.println("Arduino Serial Begin");
+//  Variables
+int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 13;                // pin to blink led at each beat
+int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
+int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
 
-    //HC-05 communication set at 9600 baud rate
-    BTserial.begin(9600);  
-    Serial.println("Bluetooth Serial Begin");
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded!
+volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat".
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
 
-    //pin 3 is set to input information from button 
-    pinMode(button, INPUT); 
+// SET THE SERIAL OUTPUT TYPE TO YOUR NEEDS
+// PROCESSING_VISUALIZER works with Pulse Sensor Processing Visualizer
+//      https://github.com/WorldFamousElectronics/PulseSensor_Amped_Processing_Visualizer
+// SERIAL_PLOTTER outputs sensor data for viewing with the Arduino Serial Plotter
+//      run the Serial Plotter at 115200 baud: Tools/Serial Plotter or Command+L
+static int outputType = SERIAL_PLOTTER;
+
+
+void setup(){
+  pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
+  pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
+  Serial.begin(115200);             // we agree to talk fast!
+  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
+   // IF YOU ARE POWERING The Pulse Sensor AT VOLTAGE LESS THAN THE BOARD VOLTAGE,
+   // UN-COMMENT THE NEXT LINE AND APPLY THAT VOLTAGE TO THE A-REF PIN
+//   analogReference(EXTERNAL);
 }
- 
-void loop()
-{
- //read value from button(pin 3) and send via Bluetooth 
- BTserial.print(digitalRead(button)); 
- 
- //read value from button(pin 3) and send to serial monitor 
- Serial.println(digitalRead(button));
 
- //delay 50milliseconds 
- delay(50);  
+
+//  Where the Magic Happens
+void loop(){
+
+    serialOutput() ;
+
+  if (QS == true){     // A Heartbeat Was Found
+                       // BPM and IBI have been Determined
+                       // Quantified Self "QS" true when arduino finds a heartbeat
+        fadeRate = 255;         // Makes the LED Fade Effect Happen
+                                // Set 'fadeRate' Variable to 255 to fade LED with pulse
+        serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.
+        QS = false;                      // reset the Quantified Self flag for next time
+  }
+
+  ledFadeToBeat();                      // Makes the LED Fade Effect Happen
+  delay(20);                             //  take a break
 }
 
+
+
+
+
+void ledFadeToBeat(){
+    fadeRate -= 15;                         //  set LED fade value
+    fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
+    analogWrite(fadePin,fadeRate);          //  fade LED
+  }
